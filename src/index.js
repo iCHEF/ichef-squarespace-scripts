12c1558d03a790346be29016142f4674ce65bb13
CJ <cijies@gmail.com>
2016-10-29 13:05:45 +0800

    [footer] Update footer contents

--- src/index.js
+++ src/index.js
@@ -5,17 +5,36 @@ import {
     updateHeaderNav
 } from './update_header';
 
+import {
+    updateFooterCompanyInfo,
+    updateFooterSocialLinks,
+    updateFooterCurrentLocale,
+    updateFooterNav
+} from './update_footer';
+
 (async function ichefSquarespaceScripts() {
     try {
         const {
+            // Header contents
             headerLogo,
-            headerNav
+            headerNav,
+            // Footer contents
+            companyInfo,
+            socialLinks,
+            currentLocale,
+            footerNav
         } = await getlocaleData() || {};
 
-        // Update header
+        // Update header contents
         if (headerLogo) updateHeaderLogo(headerLogo);
         if (headerNav) updateHeaderNav(headerNav);
+
+        // Update footer contents
+        if (companyInfo) updateFooterCompanyInfo(companyInfo);
+        if (socialLinks) updateFooterSocialLinks(socialLinks);
+        if (currentLocale) updateFooterCurrentLocale(currentLocale);
+        if (footerNav) updateFooterNav(footerNav);
     } catch (error) {
-        console.warn('no current locale data');
+        console.error(error);
     }
 }());
